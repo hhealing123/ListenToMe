0bbb9def62d337aa87511c12278a0ba644d8807f
Harvey Lee <hhealing123@gmail.com>
2022-09-27 21:02:15 -0500

    Adding new files and folders for handlebars

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router;
-const userRoutes = require('./userRoutes');
-const projectRoutes =require('./projectRoutes');
-
-router.use('/user', userRoutes);
-router.use('/projects', projectRoutes);
-
-module.exports = router;
