d277f8251f50cf61c5f9c69121acf474bdad0395
Harvey Lee <hhealing123@gmail.com>
2022-10-05 18:30:26 -0500

    Revised routing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const commentRoute = require('./commentRoutes');
 const contributorsRoutes = require('./contributors-routes');
 const userPostRoutes = require('./userPostRoutes');
-const userRoutes = require('./user-routes');
+const userRoutes = require('./userRoutes');
 
 router.use('/comment', commentRoute);
 router.use('/contributors', contributorsRoutes);
 router.use('/user_post', userPostRoutes);
-router.use('users', userRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
